60b70404c587400fd44ebb8e9086d50a6880c36c
inguk <inguk@blast-team.com>
2023-11-02 18:38:24 +0900

    feat. 레이아웃 구성

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -34,14 +34,18 @@ const Home = () => {
 
   return (
     <div className="bg-black h-screen">
-      <div className="flex flex-col items-center md:px-24 lg:px-52   ">
+      <div className="flex flex-col items-center md:px-24 lg:px-52">
         <TopNav />
-        <div className="grid grid-cols-4 sm:grid-cols gap-4">
+        <h1 className="mt-24 text-4xl sm:text-5xl md:text-7xl font-bold text-gray-200 mb-5">
+          Made for you
+        </h1>
+        <div className="grid sm:grid-cols-4 grid-cols gap-4">
           {data?.map((item, index) => (
             <MusicCard
               key={index}
               title={item.snippet.title}
               description={item.snippet.description}
+              thumbnails={item.snippet.thumbnails.default.url}
             />
           ))}
         </div>
